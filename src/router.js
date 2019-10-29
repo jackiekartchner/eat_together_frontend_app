27988217331bbfaa7e0c22d1ba4956bfd7e82f92
jackie <jacqueline.kartchner@gmail.com>
2019-10-29 11:14:15 -0700

    adds logout component

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Home from "./views/Home.vue";
 import About from "./views/About.vue";
 import Signup from "./views/Signup.vue";
 import Login from "./views/Login.vue";
+import Logout from "./views/Logout.vue";
 
 Vue.use(Router);
 
@@ -12,6 +13,7 @@ export default new Router({
     { path: "/", name: "home", component: Home },
     { path: "/about", name: "about", component: About },
     { path: "/signup", name: "signup", component: Signup },
-    { path: "/login", name: "login", component: Login }
+    { path: "/login", name: "login", component: Login },
+    { path: "/logout", name: "logout", component: Logout }
   ]
 });
