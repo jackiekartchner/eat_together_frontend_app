9cc518115619587402997b3272739a611be57d08
jackie <jacqueline.kartchner@gmail.com>
2019-11-11 19:04:42 -0800

    adds header menu bar

--- src/views/users/Show.vue
+++ src/views/users/Show.vue
@@ -1,5 +1,61 @@
 <template>
   <div class="users-show">
+    <header id="header">
+      <nav class="navbar navbar-default navbar-fixed-top menu">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button
+              type="button"
+              class="navbar-toggle collapsed"
+              data-toggle="collapse"
+              data-target="#bs-example-navbar-collapse-1"
+              aria-expanded="false"
+            >
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index-register.html"><img src="" alt="logo" /></a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right main-menu">
+              <li class="dropdown"><a href="/logout">Logout</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right main-menu">
+              <li class="dropdown"><a href="/login">Login</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right main-menu">
+              <li class="dropdown"><a href="/signup">Signup</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right main-menu">
+              <li class="dropdown"><a href="/bookings">Bookings</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right main-menu">
+              <li class="dropdown"><a href="/cravings">Cravings</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right main-menu">
+              <li class="dropdown"><a href="/users/me">Profile</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right main-menu">
+              <li class="dropdown"><a href="/">Home</a></li>
+            </ul>
+            <form class="navbar-form navbar-right hidden-sm">
+              <div class="form-group">
+                <i class="icon ion-android-search"></i>
+                <input type="text" class="form-control" placeholder="Search friends, photos, videos" />
+              </div>
+            </form>
+          </div>
+          <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+      </nav>
+    </header>
+    <!--Header End-->
     <h2>Profile Page</h2>
     <h3>Name: {{ user.full_name }}</h3>
     <p>Bio: {{ user.bio }}</p>
