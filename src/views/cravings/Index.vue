52960fd86e3d6d9fa972c25c1743ed15b189b6c7
jackie <jacqueline.kartchner@gmail.com>
2019-11-12 11:34:46 -0800

    adds navbar and footer to app.vue

--- src/views/cravings/Index.vue
+++ src/views/cravings/Index.vue
@@ -1,65 +1,119 @@
 <template>
   <div class="container">
-    <header id="header">
-      <nav class="navbar navbar-default navbar-fixed-top menu">
-        <div class="container">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
-            <button
-              type="button"
-              class="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#bs-example-navbar-collapse-1"
-              aria-expanded="false"
-            >
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index-register.html"><img src="" alt="logo" /></a>
-          </div>
-
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right main-menu">
-              <li class="dropdown"><a href="/logout">Logout</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right main-menu">
-              <li class="dropdown"><a href="/login">Login</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right main-menu">
-              <li class="dropdown"><a href="/signup">Signup</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right main-menu">
-              <li class="dropdown"><a href="/bookings">Bookings</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right main-menu">
-              <li class="dropdown"><a href="/cravings">Cravings</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right main-menu">
-              <li class="dropdown"><a href="/users/me">Profile</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right main-menu">
-              <li class="dropdown"><a href="/">Home</a></li>
-            </ul>
-            <form class="navbar-form navbar-right hidden-sm">
-              <div class="form-group">
-                <i class="icon ion-android-search"></i>
-                <input type="text" class="form-control" placeholder="Search friends, photos, videos" />
-              </div>
-            </form>
-          </div>
-          <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-      </nav>
-    </header>
-    <!--Header End-->
     <h1>All Cravings</h1>
-    <router-link v-bind:to="`/cravings/new`">
-      <button class="btn btn-warning">New Craving</button>
-    </router-link>
+    <div class="container">
+      <!-- Timeline
+      ================================================= -->
+      <div class="timeline">
+        <div class="timeline-cover">
+          <!--Timeline Menu for Large Screens-->
+          <div class="timeline-nav-bar hidden-sm hidden-xs">
+            <div class="row">
+              <div class="col-md-3">
+                <div class="profile-info">
+                  <img src="http://placehold.it/300x300" alt="" class="img-responsive profile-photo" />
+                  <h3>Sarah Cruiz</h3>
+                  <p class="text-muted">Creative Director</p>
+                </div>
+              </div>
+              <div class="col-md-9">
+                <ul class="list-inline profile-menu">
+                  <li><button class="btn-primary">Add Friend</button></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!--Timeline Menu for Large Screens End-->
+
+          <!--Timeline Menu for Small Screens-->
+          <div class="navbar-mobile hidden-lg hidden-md">
+            <div class="profile-info">
+              <img src="http://placehold.it/300x300" alt="" class="img-responsive profile-photo" />
+              <h4>Sarah Cruiz</h4>
+              <p class="text-muted">Creative Director</p>
+            </div>
+            <div class="mobile-menu">
+              <ul class="list-inline">
+                <li><a href="timline.html">Timeline</a></li>
+                <li><a href="timeline-about.html">About</a></li>
+                <li><a href="timeline-album.html">Album</a></li>
+                <li><a href="timeline-friends.html" class="active">Friends</a></li>
+              </ul>
+              <button class="btn-primary">Add Friend</button>
+            </div>
+          </div>
+          <!--Timeline Menu for Small Screens End-->
+        </div>
+        <div id="page-contents">
+          <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-7">
+              <!-- Friend List
+              ================================================= -->
+              <div class="friend-list">
+                <div class="row">
+                  <div class="col-md-6 col-sm-6">
+                    <div class="friend-card">
+                      <img src="http://placehold.it/1030x360" alt="profile-cover" class="img-responsive cover" />
+                      <div class="card-info">
+                        <img src="http://placehold.it/300x300" alt="user" class="profile-photo-lg" />
+                        <div class="friend-info">
+                          <a href="#" class="pull-right text-green">My Friend</a>
+                          <h5><a href="timeline.html" class="profile-link">Sophia Lee</a></h5>
+                          <p>Student at Harvard</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-2 static">
+              <div id="sticky-sidebar">
+                <h4 class="grey">Sarah's activity</h4>
+                <div class="feed-item">
+                  <div class="live-activity">
+                    <p>
+                      <a href="#" class="profile-link">Sarah</a>
+                      Commended on a Photo
+                    </p>
+                    <p class="text-muted">5 mins ago</p>
+                  </div>
+                </div>
+                <div class="feed-item">
+                  <div class="live-activity">
+                    <p>
+                      <a href="#" class="profile-link">Sarah</a>
+                      Has posted a photo
+                    </p>
+                    <p class="text-muted">an hour ago</p>
+                  </div>
+                </div>
+                <div class="feed-item">
+                  <div class="live-activity">
+                    <p>
+                      <a href="#" class="profile-link">Sarah</a>
+                      Liked her friend's post
+                    </p>
+                    <p class="text-muted">4 hours ago</p>
+                  </div>
+                </div>
+                <div class="feed-item">
+                  <div class="live-activity">
+                    <p>
+                      <a href="#" class="profile-link">Sarah</a>
+                      has shared an album
+                    </p>
+                    <p class="text-muted">a day ago</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div v-for="craving in cravings">
       <h3>Cuisine Type: {{ craving.category }}</h3>
       <p>Price: {{ craving.price }}</p>
@@ -91,90 +145,6 @@
         </div>
       </div>
     </div>
-  </br>
-    <!-- Footer
-        ================================================= -->
-    <footer id="footer">
-      <div class="container">
-        <div class="row">
-          <div class="footer-wrapper">
-            <div class="col-md-2 col-sm-2">
-              <a href=""><img src="" alt="" class="footer-logo" /></a>
-              <ul class="list-inline social-icons">
-                <li>
-                  <a href="#"><i class="icon ion-social-facebook"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="icon ion-social-twitter"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="icon ion-social-googleplus"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="icon ion-social-pinterest"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="icon ion-social-linkedin"></i></a>
-                </li>
-              </ul>
-            </div>
-            <div class="col-md-3 col-sm-3">
-              <h6>For Foodies</h6>
-              <ul class="footer-links">
-                <li>
-                  <a href="https://bucketlistjourney.net/things-every-foodie-must-do-in-their-lifetime/">
-                    Foodie Bucket List
-                  </a>
-                </li>
-                <li>
-                  <a href="https://travel.usnews.com/rankings/best-foodie-destinations-in-the-usa/">
-                    Best Foodie Cities in the U.S.
-                  </a>
-                </li>
-                <li>
-                  <a href="https://www.rd.com/culture/best-foodie-cities-in-the-world/">
-                    Best Foodie Cities in the World
-                  </a>
-                </li>
-                <li><a href="http://www.foodtimeline.org/">Food Timeline</a></li>
-              </ul>
-            </div>
-            <div class="col-md-3 col-sm-3">
-              <h6>Best Food Blogs to Follow</h6>
-              <ul class="footer-links">
-                <li><a href="https://www.fiction-food.com/">Fiction-Food Cafe</a></li>
-                <li><a href="https://pinchofyum.com/">Pinch of Yum</a></li>
-                <li>
-                  <a href="https://chocolatecoveredkatie.com/">Chocolate Covered Katie</a>
-                </li>
-                <li><a href="https://howtofeedaloon.com/">How to feed a loon</a></li>
-                <li><a href="https://www.thefoodieaffair.com/">The Foodie Affair</a></li>
-              </ul>
-            </div>
-            <div class="col-md-4 col-sm-4">
-              <h6>Contact Us</h6>
-              <ul class="contact">
-                <li>
-                  <i class="icon ion-ios-telephone-outline"></i>
-                  +1 (800) 111 0000
-                </li>
-                <li>
-                  <i class="icon ion-ios-email-outline"></i>
-                  info@eat_together.com
-                </li>
-                <li>
-                  <i class="icon ion-ios-location-outline"></i>
-                  228 Eat Ave Seattle, WA USA
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="copyright">
-        <p>copyright @thunder-team 2016. All rights reserved</p>
-      </div>
-    </footer>
   </div>
 </template>
 
@@ -190,7 +160,7 @@ export default {
   created: function() {
     axios.get("/api/cravings").then(response => {
       this.cravings = response.data;
-      console.log(this.cravings);
+      console.log("cravings", response.data);
     });
     console.log(moment("2019-11-12T13:00:00.000Z").format());
   },
